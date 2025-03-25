c1f81c642c0f0c4a7cae79e52356352273a47027
Yakovyakov <yyak0423@gmail.com>
2025-03-25 08:58:53 -0400

    t more test for generic items

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -13,11 +13,31 @@ describe("Gilded Rose", () => {
 
   [
     { 
-      description: "generic item: quality dicrease to 0 if sellIn <= 0",
+      description: "generic item: quality is decrease to 0 if sellIn <= 0",
       input: { name: "foo", sellIn: 0, quality: 2 },
       expected: { name: "foo", sellIn: -1, quality: 0 }
     },
-].forEach(({ description, input, expected }) => {
+    { 
+      description: "generic item: quality does not change if it is 0",
+      input: { name: "foo", sellIn: 0, quality: 0 },
+      expected: { name: "foo", sellIn: -1, quality: 0 }
+    },
+    { 
+      description: "generic item: quality is decrease by one if sellIn > 0",
+      input: { name: "foo", sellIn: 1, quality: 1 },
+      expected: { name: "foo", sellIn: 0, quality: 0 }
+    },
+    { 
+      description: "generic item: quality is decrease by two if sellIn <= 0",
+      input: { name: "foo", sellIn: 0, quality: 3 },
+      expected: { name: "foo", sellIn: -1, quality: 1 }
+    },
+    { 
+      description: "generic item: testing extreme values",
+      input: { name: "foo", sellIn: -10, quality: 50 },
+      expected: { name: "foo", sellIn: -11, quality: 48 }
+    },
+  ].forEach(({ description, input, expected }) => {
     test(description, () => {
       const gildedRose = new Shop([new Item(input.name, input.sellIn, input.quality)]);
       const items = gildedRose.updateQuality();
