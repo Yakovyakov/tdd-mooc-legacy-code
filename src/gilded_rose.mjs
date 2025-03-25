0fb1be5a94d09f80cbd5f979e97be43cef870002
Yakovyakov <yyak0423@gmail.com>
2025-03-25 13:34:13 -0400

    r inline and remove function

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -25,52 +25,43 @@ export class Shop {
       return;
     }
     if (item.name === "Backstage passes to a TAFKAL80ETC concert"){
-      this.updateEveryThingElse(item);
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+        if (item.sellIn < 11) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+        if (item.sellIn < 6) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+        
+      }
+      item.sellIn = item.sellIn - 1;
+      if (item.sellIn < 0) {
+        item.quality = item.quality - item.quality;
+      }
     } else {
-      this.updateEveryThingElse(item);
-    }
-  }
-
-  updateEveryThingElse(item) {
-    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
       if (item.quality > 0) {
         if (item.name != "Sulfuras, Hand of Ragnaros") {
           item.quality = item.quality - 1;
         }
       }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
+      if (item.name != "Sulfuras, Hand of Ragnaros") {
+        item.sellIn = item.sellIn - 1;
       }
-    }
-    if (item.name != "Sulfuras, Hand of Ragnaros") {
-      item.sellIn = item.sellIn - 1;
-    }
-    if (item.sellIn < 0) {
-      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+      if (item.sellIn < 0) {
         if (item.quality > 0) {
           if (item.name != "Sulfuras, Hand of Ragnaros") {
             item.quality = item.quality - 1;
           }
         }
-      } else {
-        item.quality = item.quality - item.quality;
       }
-
     }
   }
+
 
   updateAgedBrie(item) {
     if (item.quality < 50) {
