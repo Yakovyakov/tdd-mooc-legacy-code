e52c8d2bae548027f916877bf187410a931d496d
Yakovyakov <yyak0423@gmail.com>
2025-03-25 12:31:34 -0400

    r remove red

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -20,44 +20,19 @@ export class Shop {
   }
 
   updateSingleItem(item) {
-    if (true){
-      if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
-        }
+    if (item.name === "Aged Brie"){
+      if (false && true) {
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
+
         }
       }
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
+      if (true) {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
+        if (false) {
         } else {
           if (item.quality < 50) {
             item.quality = item.quality + 1;
