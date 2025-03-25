993c73f1f1eaf4069594eb9d366f32b379f9ff2c
Yakovyakov <yyak0423@gmail.com>
2025-03-25 14:10:19 -0400

    r extract method increaseQuality

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -62,7 +62,6 @@ export class Shop {
           item.quality = item.quality + 1;
         }
       }
-
     }
     item.sellIn = item.sellIn - 1;
     if (item.sellIn < 0) {
@@ -71,15 +70,19 @@ export class Shop {
   }
 
   updateAgedBrie(item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
+    this.increaseQuality(item);
     item.sellIn = item.sellIn - 1;
 
     if (item.sellIn < 0) {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
       }
+    }
+  }
+
+  increaseQuality(item) {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
     }
   }
 }
