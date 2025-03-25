c14424b561c93dde842e9ec90d6b3c87436be199
Yakovyakov <yyak0423@gmail.com>
2025-03-25 14:11:55 -0400

    r delete duplicate code

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -74,9 +74,7 @@ export class Shop {
     item.sellIn = item.sellIn - 1;
 
     if (item.sellIn < 0) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
+      this.increaseQuality(item);
     }
   }
 
