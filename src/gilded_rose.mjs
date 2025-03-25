34b84fa2dc80c0d65c00fe66bbd6d87389860c5d
Yakovyakov <yyak0423@gmail.com>
2025-03-25 14:15:45 -0400

    r reorder function updateBackstagePasses

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -50,20 +50,16 @@ export class Shop {
   }
 
   updateBackstagePasses(item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-      if (item.sellIn < 11) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
-      if (item.sellIn < 6) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
+    this.increaseQuality(item);
+    if (item.sellIn < 11) {
+        this.increaseQuality(item);
     }
+    if (item.sellIn < 6) {
+      this.increaseQuality(item);
+    }
+    
     item.sellIn = item.sellIn - 1;
+    
     if (item.sellIn < 0) {
       item.quality = item.quality - item.quality;
     }
