0b86692441036076985df076d39ed20ee2b03967
Yakovyakov <yyak0423@gmail.com>
2025-03-25 13:36:04 -0400

    r extract method updateBackstagePasses

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -25,24 +25,7 @@ export class Shop {
       return;
     }
     if (item.name === "Backstage passes to a TAFKAL80ETC concert"){
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        
-      }
-      item.sellIn = item.sellIn - 1;
-      if (item.sellIn < 0) {
-        item.quality = item.quality - item.quality;
-      }
+      this.updateBackstagePasses(item);
     } else {
       if (item.quality > 0) {
         if (item.name != "Sulfuras, Hand of Ragnaros") {
@@ -62,6 +45,27 @@ export class Shop {
     }
   }
 
+
+  updateBackstagePasses(item) {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+      if (item.sellIn < 11) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
+      if (item.sellIn < 6) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
+
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+      item.quality = item.quality - item.quality;
+    }
+  }
 
   updateAgedBrie(item) {
     if (item.quality < 50) {
