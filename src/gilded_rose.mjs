259cacb7fb3fb4433e343944ea12dac2182308f9
Yakovyakov <yyak0423@gmail.com>
2025-03-25 13:58:31 -0400

    r extract method updateEveryThingElse

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -30,20 +30,24 @@ export class Shop {
     }
     if (item.name === "Sulfuras, Hand of Ragnaros") {
       return;
-    } else {
-      if (item.quality > 0) {
-        item.quality = item.quality - 1;
-      }
-      item.sellIn = item.sellIn - 1;
-      if (item.sellIn < 0) {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1;
-        }
-      }
     }
+
+    this.updateEveryThingElse(item);
   }
 
 
+
+  updateEveryThingElse(item) {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+      if (item.quality > 0) {
+        item.quality = item.quality - 1;
+      }
+    }
+  }
 
   updateBackstagePasses(item) {
     if (item.quality < 50) {
