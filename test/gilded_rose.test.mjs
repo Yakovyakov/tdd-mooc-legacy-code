fce11dd116e00669a5fe1d1c31e842bd38744913
Yakovyakov <yyak0423@gmail.com>
2025-03-25 08:20:10 -0400

    t generic item

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -9,6 +9,23 @@ describe("Gilded Rose", () => {
     expect(items[0].name).to.equal("foo");
     expect(items[0].sellIn).to.equal(-1);
     expect(items[0].quality).to.equal(0);
-
   });
+
+  [
+    { 
+      description: "generic item: quality dicrease to 0 if sellIn <= 0",
+      input: { name: "foo", sellIn: 0, quality: 2 },
+      expected: { name: "foo", sellIn: -1, quality: 0 }
+    },
+].forEach(({ description, input, expected }) => {
+    test(description, () => {
+      const gildedRose = new Shop([new Item(input.name, input.sellIn, input.quality)]);
+      const items = gildedRose.updateQuality();
+      
+      expect(items[0].name).to.equal(expected.name);
+      expect(items[0].sellIn).to.equal(expected.sellIn);
+      expect(items[0].quality).to.equal(expected.quality);
+    });
+});
+
 });
