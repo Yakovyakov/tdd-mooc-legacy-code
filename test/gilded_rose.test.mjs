77f359ebb3728eff710c7eaec5e0e553059d2b78
Yakovyakov <yyak0423@gmail.com>
2025-03-25 08:16:20 -0400

    t a better test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -7,5 +7,8 @@ describe("Gilded Rose", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(0);
+
   });
 });
