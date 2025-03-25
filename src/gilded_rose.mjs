608a06566f533baa3d83899ec9b3db8f26e21899
Yakovyakov <yyak0423@gmail.com>
2025-03-25 12:39:16 -0400

    r refactored

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -21,29 +21,21 @@ export class Shop {
 
   updateSingleItem(item) {
     if (item.name === "Aged Brie"){
-      if (false && true) {
-      } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+      item.sellIn = item.sellIn - 1;
+      
+      if (item.sellIn < 0) {
+      
         if (item.quality < 50) {
           item.quality = item.quality + 1;
-
         }
       }
-      if (true) {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (false) {
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-        
     }else{
 
 
-      if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
         if (item.quality > 0) {
           if (item.name != "Sulfuras, Hand of Ragnaros") {
             item.quality = item.quality - 1;
@@ -70,23 +62,17 @@ export class Shop {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
-              }
+        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+          if (item.quality > 0) {
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+              item.quality = item.quality - 1;
             }
-          } else {
-            item.quality = item.quality - item.quality;
           }
         } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+          item.quality = item.quality - item.quality;
         }
+        
       }
-     }
+    }
   }
-
 }
